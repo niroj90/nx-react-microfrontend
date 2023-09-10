e5ae9c2994a36c1e8cea10fb2dcb85d22e2be923
Niroechkumar Sugumar <snero90@gmail.com>
2023-09-10 13:30:48 +0800

    projects added

---
+++ micro-fronend-test/ecommerce/src/app/app.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import NxWelcome from './nx-welcome';
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+const ElectronicFashionHome = React.lazy(
+  () => import('electronic-fashion-home/Module')
+);
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/electronic-fashion-home">ElectronicFashionHome</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="ecommerce" />} />
+
+        <Route
+          path="/electronic-fashion-home"
+          element={<ElectronicFashionHome />}
+        />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
---
+++ micro-fronend-test/electronic-fashion-home/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'electronic-fashion-home',
+  exposes: {
+    './Module': './src/remote-entry.ts',
+  },
+};
